abbec5345d7a15e6ae45c9373bb010395ac5160e
Kimfucious <kim@kimford.me>
2023-06-05 20:15:10 +0700

    add action type

--- src/types/actions.ts
+++ src/types/actions.ts
@@ -2,5 +2,6 @@ export enum ActionType {
     ABANDON_CHANGES = "abandon_changes",
     SAVE_CHANGES = "save_changes",
     SET_DRAFT_ACTIVE_SCENE_TEXT = "set_draft_active_scene_text",
+    SET_IS_DIFFING = "set_is_diffing",
     SET_ID = "set_id"
 }
